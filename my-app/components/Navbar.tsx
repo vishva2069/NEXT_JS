407c6272d0292e5fe50bf75501e46f464482d74f
vishva2069 <vishvapansuriya.2004@gmail.com>
2026-03-19 13:27:22 +0530

    th

---
+++ my-app/components/Navbar.tsx
@@ -0,0 +1,8 @@
+export default function Navbar() {
+  return (
+    <div className=" text-2xl text-center mt-0.5 py-1 p-4  border-l-gray-300 border-2 text-blue-400  ">
+      <h1 className="text-white-900 p-7"> To a discript of data to like a key words styling performane but configuration</h1>
+      <button className="bg-blue-700 text-white px-4 py-2 rounded">Login</button>
+    </div>
+  )
+}
